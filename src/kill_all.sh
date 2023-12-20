e3ba329d19eecc3c178c69990df14bf33e62895b
lvqs673 <lv.qingsong@foxmail.com>
2023-12-21 01:44:01 +0800

    删除分布式训练的进程

---
+++ src/kill_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 该脚本是为了杀死几个结点的进程
+
+target_dir="/data/lvqs/ToyDDP"
+src_file="./src/comm.py"
+
+HOSTS=("10.103.10.158" "10.103.10.157" "10.103.11.151")
+
+USER="lvqs"
+
+
+for ((RANK=0;RANK<${#HOSTS[@]};RANK++))
+do
+    ( 
+        HOST=${HOSTS[$RANK]} ; \
+        ssh $USER@$HOST "pkill -f python.*src.*py$"
+    )&
+done
+
+
+wait
